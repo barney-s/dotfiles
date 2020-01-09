71a02d10fab6ffcfedea71459d54dba72b66e645
Barni S <bharanis78@gmail.com>
2020-01-09 10:41:55 -0800

    Adding dev setup scripts

---
+++ dev_setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#set -x
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+setup_golang ()
+{
+   which go || error "Please install golang. https://golang.org/doc/install"
+   grep GOPATH ~/.bash_profile || echo 'export GOPATH=${HOME}/workspace' >> ~/.bash_profile
+}
+
+clone_github_repo ()
+{
+  mkdir -p $1
+  cd $1
+  if [ -d $1/$2 ]; then
+    echo "$1/$2 already exists. Not cloning $3"
+  else
+    git clone $3
+  fi
+  if [ "$4" != "" ]; then
+     cd $1/$2
+     git remote add upstream $4 && echo "Set upstream $4" || echo "upstream already exists"
+  fi
+}
+
+setup_k8s_dev_env ()
+{
+  clone_github_repo $GOPATH/src/k8s.io kubernetes git@github.com:barney-s/kubernetes.git git@github.com:kubernetes/kubernetes.git
+  clone_github_repo $GOPATH/src/k8s.io enhancements git@github.com:barney-s/enhancements.git git@github.com:kubernetes/enhancements.git
+}
+
+setup_golang
+setup_k8s_dev_env
--- new_mac_setup.sh
+++ new_mac_setup.sh
@@ -11,6 +11,9 @@ function install_basic_sw {
   brew install tmux
   brew install git-review
   brew install ctags
+
+  python -m pip install --user --upgrade pynvim
+  python3 -m pip install --user --upgrade pynvim
 }
 
 function install_nerd_fonts {
@@ -19,10 +22,14 @@ function install_nerd_fonts {
   brew cask install font-dejavusansmono-nerd-font
 }
 
-
+function install_pip {
+   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+   sudo python get-pip.py
+}
 
 # --- main ------------------------
 install_brew
+install_pip
 install_basic_sw
 install_nerd_fonts
 
